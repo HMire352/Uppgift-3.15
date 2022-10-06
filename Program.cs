0470bea1b987aa2a29868615b9b5eff89165fc39
Pingvu <alka04006@utb.vaxjo.se>
2022-10-06 13:25:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Uppgift_3._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra?");
+            int timmar = int.Parse(Console.ReadLine());
+            int h = 80;
+
+            if (timmar <=11 )
+
+            {
+                Console.WriteLine(timmar * h + "kr");
+
+               
+            }
+            else if (timmar >= 12)
+            {
+                Console.WriteLine("Nej, du får inte hyra bilen mer än 11 timmar per dag");
+
+            }
+ }   }   }
